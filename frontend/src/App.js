5a6b1f14cd802728015f9b4b65b8ac7e11a86c0d
dzania <artur.kantorczyk@gmail.com>
2021-07-14 14:26:44 +0200

    created navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { CssBaseline, cssBaseline } from "@material-ui/core";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: "100vh",
+    maxHeight: "80vh",
+    backgroundImage: `url(${process.env.PUBLIC_URL + "/assets/wave1.svg"})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
     </div>
   );
 }
