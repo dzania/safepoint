ce86ea5f2d8c394fcb866717edd114c34a13a3b6
dzania <artur.kantorczyk@gmail.com>
2021-08-05 15:11:25 +0200

    Made separate components for navbar based on authentication

---
+++ frontend/src/components/Credentials.js
@@ -0,0 +1,9 @@
+export default function Credentials() {
+  return (
+    <div class="container">
+      <div class="card">
+        <div class="card-body"></div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -1,84 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { Navbar, Nav } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.css";
-import { Link } from "react-router-dom";
-import AuthService from "../services/auth-service.js";
+import NavbarAuth from './NavbarAuth'
+import NavbarUnauth from './NavbarUnauth'
 
 export default function Navigation() {
-  const [currentUser, setCurrentUser] = useState(false);
-
-  useEffect(() => {
-    const user = AuthService.getCurrentUser();
-
-    if (user) {
-      setCurrentUser(user);
-    }
-  }, []);
-
-  const logOut = () => {
-    AuthService.logout();
-    setCurrentUser(false);
-  };
-
+  // Change navigation based on user authentication
   return (
     <div>
       {currentUser ? (
-        <Navbar expand="lg" variant="dark" bg="none">
-          <Container>
-            <Navbar.Brand
-              as={Link}
-              to={"/"}
-              style={{
-                fontSize: "4rem",
-                color: "#fff",
-                position: "sticky",
-                fontWeight: 500,
-              }}
-            >
-              Safe<span style={{ color: "#00FF48" }}>Point.</span>
-            </Navbar.Brand>
-
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse className="justify-content-end">
-              <Nav>
-                <Nav.Link as={Link} to={"/"} onClick={logOut}>
-                  Logout
-                </Nav.Link>
-                <Nav.Link>{currentUser.username}</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <NavbarAuth/>
       ) : (
-        <Navbar expand="lg" variant="dark" bg="none">
-          <Container>
-            <Navbar.Brand
-              as={Link}
-              to={"/"}
-              style={{
-                fontSize: "4rem",
-                color: "#fff",
-                position: "sticky",
-                fontWeight: 500,
-              }}
-            >
-              Safe<span style={{ color: "#00FF48" }}>Point.</span>
-            </Navbar.Brand>
+        <NavbarUnauth/>
 
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse className="justify-content-end">
-              <Nav>
-                <Nav.Link as={Link} to={"/login"}>
-                  Login
-                </Nav.Link>
-                <Nav.Link as={Link} to={"/register"}>
-                  Register
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
       )}
     </div>
   );
